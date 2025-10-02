d35321c3b47791c4ceb7f9fc77c1f60f8d4d5fdd
redriot88 <166695662+redriot88@users.noreply.github.com>
2025-10-02 10:26:10 +1000

    Create main.c
    created main.c

---
+++ main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include locker.c
+
+/*******************************************************************************
+Main
+*******************************************************************************/
+int main(void) {
+    int choice = 0;
+    char input[10];
+
+  /*
+  if (!checkMasterPIN()){
+    printf("Access denied.\n");
+    return 1;
+  */
+  
+
+    while (1) {
+        printMenu();
+        printf("Enter your PIN>\n"); /*either PIN or we can have no ask for masterpassword*/
+        fflush(stdout);
+
+        if (scanf("%d", &choice) != 1) {
+            return 0; /*or we can use continue i.e.
+            printf("Invalid input.\n");
+            continue;
+            */
+        }
+        (void)getchar(); /* clear newline */
+        /*
+        if (strcmp(input, "q") == 0 || strcmp(input, "Q") == 0 {
+        printf("Exiting Personal Document Locker.\n");
+        break;
+        */
+
+        /* 
+        choice = atoi(input); 
+        */
+
+        switch (choice) {
+            case 1: addFile(); break;
+            case 2: extractFile(); break;
+            case 3: removeFile(); break;
+            case 4: listFile(); break;
+            case 5: searchFile(); break;
+            case 6: changePIN(); break;
+            case 7: quit(); return 0; /* we'll need atoi and read in q for 7 to quit, but case 7 may be redundant*/
+            default: printf("Invalid choice.\n");
+        }
+    }
+    return 0;
+}
