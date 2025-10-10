b96269ae827fbcd68582f9b3891e9fd08c41bd4b
redriot88 <166695662+redriot88@users.noreply.github.com>
2025-10-10 15:46:10 +1100

    Update storage.h
    will update later

--- storage.h
+++ storage.h
@@ -2,8 +2,18 @@
 #define STORAGE_H
 #include "locker.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_FILENAME_LENGTH 64
+#define MAX_FILES 50
+
+typedef struct {
+  char filename[MAX_FILE_LEN];
+
 /* Placeholder function to persist full locker (data + index) */
-int storage_save_all(const char *path, const index_t *idx);
-int storage_load_all(const char *path, index_t *idx);
+int storageSaveAll(const char *path, const index_t *idx);
+int storageLoadAll(const char *path, index_t *idx);
 
 #endif /* STORAGE_H */
