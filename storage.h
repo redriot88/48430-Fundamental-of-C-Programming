04c7549c220d4007f1ff1af8f44879b0bc0a6833
redriot88 <166695662+redriot88@users.noreply.github.com>
2025-10-17 14:42:33 +1100

    Update storage.h
    adding desc

--- storage.h
+++ storage.h
@@ -13,7 +13,24 @@ typedef struct {
   char filename[MAX_FILE_LEN];
 
 /* Placeholder function to persist full locker (data + index) */
+
+/*
+Save all locker data and index to disk
+-will need path to output file
+pointer to index structure containing locker entries
+return 0 perhaps for success
+*/
+
 int storageSaveAll(const char *path, const index_t *idx);
+
+/*
+load data and index to disk
+-path to input file
+pointer to index structure to populate
+return 0 perhaps for success
+*/
+
+
 int storageLoadAll(const char *path, index_t *idx);
 
 #endif /* STORAGE_H */
